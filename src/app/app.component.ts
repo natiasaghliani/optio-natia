2c268612bea07409087841651c9557036b35e848
natiasaghliani <natiasaghliani0@gmail.com>
2023-10-15 13:05:00 +0400

    feat: finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { HttpService } from './services/http.service';
+import { MatDrawer } from '@angular/material/sidenav';
 import { Observable } from 'rxjs';
+import { HttpService } from './services/http.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,7 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'optio-natia';
+
   displayedColumns: string[] = ['image', 'name', 'active', 'zoneId', 'startDate', 'endDate', 'labels'];
 
   bannersData = null;
@@ -24,18 +25,32 @@ export class AppComponent {
 
   labelData: any = [];
 
-  uploadedImageName: string = '';
+  pageInfo = {
+    pageIndex: 0,
+    pageSize: 10,
+  }
+
+  totalPages = 0;
+
+  sortInfo = {
+    sortBy: "",
+    sortDirection: "",
+  }
+
+  searchText: string = '';
 
   bannerForm!: FormGroup;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  @ViewChild('drawer') drawer!: MatDrawer;
+
   constructor(private fb: FormBuilder, private httpService: HttpService) {
     this.bannerForm = this.fb.group({
       id: [null],
-      image: [null, Validators.required],
+      fileId: [null, Validators.required],
       name: [null, Validators.required],
-      active: [null, Validators.required],
+      active: [false],
       zoneId: [null, Validators.required],
       startDate: [null, Validators.required],
       endDate: [null],
@@ -44,41 +59,35 @@ export class AppComponent {
       language: [null, Validators.required],
       priority: [null, Validators.required],
       url: [null, Validators.required],
-      
-
     })
 
-
-    this.fetchBannersData()
-
-    this.fetchChannels()
-
-    this.fetchZones()
-
-    this.fetchLanguages()
-
-    this.fetchLabels()
-
+    this.fetchBannersData();
+    this.fetchChannels();
+    this.fetchZones();
+    this.fetchLanguages();
+    this.fetchLabels();
   }
 
+  saveForm() {
+    const url = 'https://development.api.optio.ai/api/v2/banners/save';
 
-  submit() {
-    console.log(this.bannerForm.value);
+    this.httpService.post(url, this.bannerForm.value).subscribe((response) => {
+      this.fetchBannersData();
+      this.closeDrawer();
+    })
   }
 
-  myFilter = (d: Date | null): boolean => {
-    const day = (d || new Date()).getDay();
-    return day !== 0 && day !== 6;
-  };
   private fetchBannersData(): void {
     const url = 'https://development.api.optio.ai/api/v2/banners/find'
     const data = {
-      pageIndex: 0,
-      pageSize: 20,
-
+      ...this.pageInfo,
+      ...this.sortInfo,
+      search: this.searchText
     }
+
     this.httpService.post(url, data).subscribe((response) => {
-      this.bannersData = response?.data?.entities
+      this.bannersData = response?.data?.entities;
+      this.totalPages = response?.data?.total;
     })
   }
 
@@ -86,42 +95,36 @@ export class AppComponent {
     const url = 'https://development.api.optio.ai/api/v2/banners/find-one'
     const data = {
       id: bannerId,
-
     }
+  
     this.httpService.post(url, data).subscribe((response) => {
-      this.bannerForm.patchValue(response?.data)
+      this.bannerForm.patchValue(response?.data);
     })
   }
 
   private fetchChannels(): void {
     this.fetchReferenceDatas(ReferenceDatasEnum.Channels).subscribe((response) => {
-      this.channelsData = response?.data?.entities
-
+      this.channelsData = response?.data?.entities;
     })
   }
 
   private fetchZones(): void {
     this.fetchReferenceDatas(ReferenceDatasEnum.Zones).subscribe((response) => {
-      this.zoneData = response?.data?.entities
-
+      this.zoneData = response?.data?.entities;
     })
   }
 
   private fetchLanguages(): void {
     this.fetchReferenceDatas(ReferenceDatasEnum.Languages).subscribe((response) => {
-      this.languageData = response?.data?.entities
-
+      this.languageData = response?.data?.entities;
     })
   }
 
   private fetchLabels(): void {
     this.fetchReferenceDatas(ReferenceDatasEnum.Labels).subscribe((response) => {
-      this.labelData = response?.data?.entities
-
+      this.labelData = response?.data?.entities;
     })
   }
-
-  
 
   private fetchReferenceDatas(referenceDataId: ReferenceDatasEnum): Observable<any> {
     const url = 'https://development.api.optio.ai/api/v2/reference-data/find';
@@ -141,32 +144,47 @@ export class AppComponent {
     const image = event?.target?.files?.[0];
     formData.set('blob', image);
     const url = 'https://development.api.optio.ai/api/v2/blob/upload';
-    this.httpService.post(url, formData).subscribe((response) => {
-      console.log('upload', response)
-      this.bannerForm.get('fileId')?.setValue(response?.data?.id)
-      this.uploadedImageName = response?.data?.fileName;
 
+    this.httpService.post(url, formData).subscribe((response) => {
+      this.bannerForm.get('fileId')?.setValue(response?.data?.id)
     })
 
-    console.log('ak movida', image);
+  }
+
+  pageChange(event: any): void {
+    this.pageInfo.pageIndex = event?.pageIndex;
+    this.pageInfo.pageSize = event?.pageSize;
+    this.fetchBannersData();
+  }
+
+  sortChange(event: any): void {
+    this.sortInfo.sortBy = event?.active
+    this.sortInfo.sortDirection = event?.direction
+    this.fetchBannersData();
+  }
+
+  filterText(event: any): void {
+    this.searchText = event?.target?.value;
+    this.fetchBannersData()
+  }
+
+  drawerStateChange(open: boolean): void {
+    if (!open) {
+      this.bannerForm.reset();
+    }
+  }
+
+  openDrawer(): void {
+    this.drawer?.open();
+  }
+
+  closeDrawer(): void {
+    this.drawer?.close();
   }
 
 }
 
-export interface PeriodicElement {
-  id: number;
-  image: string;
-  name: string;
-  active: boolean;
-  zoneId: number;
-  startDate: string;
-  endDate: string;
-  labels: string[];
-  
-
-}
-
-enum ReferenceDatasEnum{
+enum ReferenceDatasEnum {
   Channels = 1600,
   Zones = 1700,
   Labels = 1900,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import { HttpClientModule } from '@angular/common/http';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -31,15 +32,20 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     HttpClientModule,
     MatTableModule,
     MatPaginatorModule,
-    FormsModule, MatFormFieldModule, MatInputModule, MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
     MatButtonModule,
-    MatSelectModule,MatSidenavModule,
+    MatSelectModule,
+    MatSidenavModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatDividerModule,MatListModule,
+    MatDividerModule,
+    MatListModule,
     ReactiveFormsModule,
     MatSlideToggleModule,
-    FormsModule
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
